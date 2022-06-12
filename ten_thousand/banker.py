309a8f534b630b05f741112c83f2d3bda6cb29ad
Ahmad Khaled Zaid <ahmadk.zaid97@gmail.com>
2022-06-12 19:47:50 +0300

    create shelf method

--- ten_thousand/banker.py
+++ ten_thousand/banker.py
@@ -1,7 +1,13 @@
 class Banker:
-    pass
-###################
-# Shelf method - Ahmad
+
+    ###################
+    # Shelf method - Ahmad
+    def __init__(self):
+        self.shelved = 0
+
+    def shelf(self, un_banked_points: int):
+        self.shelved = un_banked_points
+
 ###################
 
 ###################
