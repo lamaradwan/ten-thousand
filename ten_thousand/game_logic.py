56884313de88005e28613daae8d721671f625738
Lama Radwan <lama.radwan96@gmail.com>
2022-06-13 10:39:09 +0300

    test

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -10,12 +10,6 @@ class GameLogic:
 
 ###################
 #Roll-Dice method - Lama Radwan
-    @staticmethod
-    def roll_dice(num):
-        tuple = ()
-        for i in range(0, num):
-            result = random.randint(1, 6)
-            tuple = (*tuple, result)
-        return tuple
+
 ###################
 
