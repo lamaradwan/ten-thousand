9a84f66fdfb9b339ec182750d55a9c654b3fd54c
Ahmad Khaled Zaid <ahmadk.zaid97@gmail.com>
2022-06-16 09:17:35 +0300

    change the test path to work from terminal

--- tests/test_game.py
+++ tests/test_game.py
@@ -5,29 +5,29 @@ import pytest
 
 def test_no_for_play_game():
     game = Game()
-    diffs = diff(game.play, 'version_2/quitter.sim.txt')
+    diffs = diff(game.play, 'tests/version_2/quitter.sim.txt')
     assert not diffs, diffs
 
 
 def test_one_and_done():
     game = Game()
-    diffs = diff(game.play, 'version_2/one_and_done.sim.txt')
+    diffs = diff(game.play, 'tests/version_2/one_and_done.sim.txt')
     assert not diffs, diffs
 
 
 def test_bank_one_roll_then_quit():
     game = Game()
-    diffs = diff(game.play, 'version_2/bank_one_roll_then_quit.sim.txt')
+    diffs = diff(game.play, 'tests/version_2/bank_one_roll_then_quit.sim.txt')
     assert not diffs, diffs
 
 
 def test_repeat_roller():
     game = Game()
-    diffs = diff(game.play, 'version_2/repeat_roller.sim.txt')
+    diffs = diff(game.play, 'tests/version_2/repeat_roller.sim.txt')
     assert not diffs, diffs
 
 
 def test_bank_first_for_two_rounds():
     game = Game()
-    diffs = diff(game.play, 'version_2/bank_first_for_two_rounds.sim.txt')
+    diffs = diff(game.play, 'tests/version_2/bank_first_for_two_rounds.sim.txt')
     assert not diffs, diffs
